e5e45286feadb607ff7a2ffb27c1c6633f35f400
kristinerakic16 <55459220+kristinerakic16@users.noreply.github.com>
2020-05-04 17:05:31 -0400

    Add files via upload
    Working Database Insertion

---
+++ Database.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+
+public class Database {
+
+	static Connection connection = null;
+	static String url = "jdbc:mysql://localhost:3306/VC3?useTimezone=true&serverTimezone=UTC";
+	static String username = "root";
+	static String password = "Dancer16!";
+
+	public static void insertIntoDatabase(String clientName, int clientID, String jobName, int jobID, String timestamp, int Duration) {
+		
+		try {
+			//declares a connection to your database
+			connection = DriverManager.getConnection(url, username, password);
+			//creates an insert query
+			String sql = "INSERT INTO client" + "(ClientID , name)" + " VALUES ("+ clientID +", '"+ clientName +"')";
+			String sql2 = "INSERT INTO job" + "(JobID , duration, timeSubmitted, clientID)" + " VALUES ("+ jobID +", '"+ Duration +"', "+ timestamp +", '"+ clientID +"')";
+			//establishes the connection session
+			Statement statement = connection.createStatement();
+			//executes the query 
+			int row = statement.executeUpdate(sql);
+			//the return value is the indication of success or failure of the query execution
+			if (row > 0)
+				System.out.println("Client Data was inserted!");
+				row = statement.executeUpdate(sql2);
+			//the return value is the indication of success or failure of the query execution
+			if (row > 0)
+				System.out.println("Job Data was inserted!");
+
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.getMessage();
+		}
+	}
+}
---
+++ VC3.sql
@@ -0,0 +1,78 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: VC3
+-- ------------------------------------------------------
+-- Server version	8.0.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `ClientID` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`ClientID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (0,'null');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job`
+--
+
+DROP TABLE IF EXISTS `job`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `job` (
+  `JobID` int(11) NOT NULL,
+  `duration` int(45) NOT NULL,
+  `timeSubmitted` varchar(45) NOT NULL,
+  `ClientID` int(11) NOT NULL,
+  PRIMARY KEY (`JobID`),
+  KEY `ClientID` (`ClientID`),
+  CONSTRAINT `job_ibfk_1` FOREIGN KEY (`ClientID`) REFERENCES `client` (`ClientID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job`
+--
+
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-04 16:39:44
