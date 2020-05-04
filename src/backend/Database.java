d2c158fc6c9711c7667d0932cebd496dd48eeb02
yjin8 <yuchenj1998@gmail.com>
2020-05-04 18:20:56 -0400

    added package statements

--- src/backend/Database.java
+++ src/backend/Database.java
@@ -1,3 +1,5 @@
+package backend; 
+
 import java.sql.*;
 
 public class Database {
--- src/backend/DatabaseInsert.java
+++ src/backend/DatabaseInsert.java
@@ -1,3 +1,5 @@
+package backend;
+
 public class DatabaseInsert extends Database{
 	public static void main (String [] args) {
 	insertIntoDatabase(null, 0, null, 0, null, 0);
